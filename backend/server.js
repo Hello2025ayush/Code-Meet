47844fbac35e2806f6421646c45c21e6c6f1f9c5
Hello2025ayush <ayushtalrejawillrock@gmail.com>
2026-03-24 23:06:04 +0530

    updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,13 @@ server.listen(PORT, () => {                                    // .listen starts
     console.log(`Server is running on PORT: ${PORT}`);
 });
 
- 
-
 app.use(errorHandler);
+
+// frontend connection for deployment..
+import path from "path";
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
